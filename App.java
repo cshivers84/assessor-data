5e639a38acca0d067893de3ea09538867d30397f
cshivers84 <cshivers84@users.noreply.github.com>
2017-12-14 22:43:04 -0600

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-package org.assessordata.batch.assessordata_batch;
-
-public class App {
-    public static void main( String[] args ) {
-        if (FileHandler.updateFile()) {
-        	FileHandler.updateData();
-        }
-    }
-}
